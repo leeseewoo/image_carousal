fd146b29c361cea2e58e495cd80ff696fc3cd280
leeseewoo <leeseewoo@gmail.com>
2024-04-30 14:44:46 +0900

    Done!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:image_carousal/screen/HomeScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ),);
 }
 
 class MyApp extends StatelessWidget {
